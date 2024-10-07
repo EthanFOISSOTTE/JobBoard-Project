d96956af3051f0e549e049d82cbaa94461a07846
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2024-10-07 12:43:22 +0200

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -9,7 +9,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // Configurer PDO pour qu’il génère des exceptions en cas d’erreurs
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie";
 } catch(PDOException $e) {
     echo "La connexion a échoué : " . $e->getMessage();
 }

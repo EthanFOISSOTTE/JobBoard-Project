7d4f361d3c4dbdee2a02583b941da38affb79b7e
Pol-Mattis HARQUET <pol-mattis.harquet@epitech.eu>
2024-10-07 13:26:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
 	<title>Job Board</title>
 </head>
 <body>
+	<?php
+	include 'variables-bdd.php';
 
+	try {
+		// Création d'une nouvelle instance PDO
+		$bdd = new PDO("mysql:host=$bdd_host;dbname=$bdd_name;charset=utf8", $bdd_user, $bdd_pass);
+		// Définir le mode d'erreur PDO sur Exception
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// Connexion réussie à la base de données
+		echo "Connexion réussie à la base de données.";
+	} catch (PDOException $e) {
+		// En cas d'erreur, affichage du message
+		die("Erreur : " . $e->getMessage());
+	}
+	?>
 </body>
 </html>
